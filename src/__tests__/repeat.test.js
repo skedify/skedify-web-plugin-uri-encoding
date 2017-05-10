192b7a0b07389ec1ce2f363cbfaa25a27c8f6156
Demian Dekoninck <demian@live.be>
2017-05-10 20:27:13 +0200

    test: add more tests for repeat

--- src/__tests__/repeat.test.js
+++ src/__tests__/repeat.test.js
@@ -1,46 +1,59 @@
 import repeat from '../repeat';
 
+const createStringWithoutRepeat = string => {
+  const aString = new String(string);
+  aString.repeat = undefined;
+
+  return aString;
+};
+
+const testCases = [
+  { string: 'abc', toBe: '' },
+  { string: 'abc', count: undefined, toBe: '' },
+  { string: 'abc', count: null, toBe: '' },
+  { string: 'abc', count: false, toBe: '' },
+  { string: 'abc', count: NaN, toBe: '' },
+  { string: 'abc', count: 'abc', toBe: '' },
+  { string: 'abc', count: -0, toBe: '' },
+  { string: 'abc', count: +0, toBe: '' },
+  { string: 'abc', count: 1, toBe: 'abc' },
+  { string: 'abc', count: 2, toBe: 'abcabc' },
+  { string: 'abc', count: 3, toBe: 'abcabcabc' },
+  { string: 'abc', count: 4, toBe: 'abcabcabcabc' },
+];
+
 describe('repeat (when String.repeat exist)', () => {
-
-  test('should return aWord five times', () => {
-    expect(repeat('aWord', 5)).toMatchSnapshot();
+  testCases.forEach(testCase => {
+    test(`should return ${testCase.toBe} on string ${testCase.string} with count ${testCase.count}`, () => {
+      expect(repeat(testCase.string, testCase.count)).toBe(testCase.toBe);
+    });
   });
-
-  test('should return empty string when empty string given', () => {
-    expect(repeat('', 5)).toMatchSnapshot();
-  });
-
-  test('should return empty string when count is null', () => {
-    expect(repeat('aWord', 0)).toMatchSnapshot();
-  });
-
 });
 
-const aString = new String('aWord');
-const aEmptyString = new String('');
-
-aString.repeat = undefined;
-aEmptyString.repeat = undefined;
-
 describe('repeat (when String.repeat does not exist)', () => {
-
-  test('should return aWord five times', () => {
-    expect(repeat(aString, 5)).toMatchSnapshot();
+  testCases.forEach(testCase => {
+    test(`should return ${testCase.toBe} on string ${testCase.string} with count ${testCase.count}`, () => {
+      expect(
+        repeat(createStringWithoutRepeat(testCase.string), testCase.count),
+      ).toBe(testCase.toBe);
+    });
   });
 
-  test('should return empty string when empty string given', () => {
-    expect(repeat(aEmptyString, 5)).toMatchSnapshot();
+  test('should throw a range error when count is negative', () => {
+    expect(() => {
+      repeat(createStringWithoutRepeat('abc'), -1);
+    }).toThrowErrorMatchingSnapshot();
   });
 
-  test('should return empty string when count is null', () => {
-    expect(repeat(aString, 0)).toMatchSnapshot();
+  test('should throw a range error when count is negative infinity', () => {
+    expect(() => {
+      repeat(createStringWithoutRepeat('abc'), -Infinity);
+    }).toThrowErrorMatchingSnapshot();
   });
 
-  test('should throw error when count is negative', () => {
-    expect(() => repeat(aString, -2)).toThrowErrorMatchingSnapshot();
-  });
-
-  test('should throw error when count is Infinity', () => {
-    expect(() => repeat(aString, Infinity)).toThrowErrorMatchingSnapshot();
+  test('should throw a range error when count is infinity', () => {
+    expect(() => {
+      repeat(createStringWithoutRepeat('abc'), +Infinity);
+    }).toThrowErrorMatchingSnapshot();
   });
 });
